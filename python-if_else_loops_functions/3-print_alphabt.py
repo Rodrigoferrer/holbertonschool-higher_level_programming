7e10aee5da404290ac42fa337dc50168a80cbfdf
Rodrigo <rodrigoferrer30@gmail.com>
2025-01-16 12:46:26 -0300

    3-print_alphabt.py

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 for i in range(97, 123):
-    if i == 101 or i == 113:
-        i = i + 1
-        i = i - 1
-    print("{:c}".format(i), end="")
-
+    if i != 101 and i != 113:
+        print("{:c}".format(i), end="")
