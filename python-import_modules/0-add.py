c594898eecb30f54d3c0d633bf3c2ef48e5770b0
Rodrigo <rodrigoferrer30@gmail.com>
2025-01-17 14:08:41 -0300

    0

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 from add_0 import add
-a = 1
-b = 2
-res = add(a, b)
-print(f"{a} + {b} = {res}")
+def main():
+    a = 1
+    b = 2
+    res = add(a, b)
+    print(f"{a} + {b} = {res}")
+
+if __name__ == "__main__":
+    main()
