7b74aa4caa2957cbec12f279ca75fc17295d718c
Rodrigo <rodrigoferrer30@gmail.com>
2025-01-23 12:18:57 -0300

    6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            print("{}{}".format(i, j))
-
+    # iterate and store smaller lists into i
+    for i in matrix:
+        # iterate over list in i storing each value
+        # into j
+        for j in i:
+            # condition for when end is not reached
+            if j != i[-1]:
+                print("{}".format(j), end=" ")
+            # condition for when end IS reached
+            else:
+                # when printing without end arg, end is a line break by
+                # default
+                print("{}".format(j))        
