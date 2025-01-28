01daa4ad09088eed537eca424fb5b6f08315807e
Rodrigo <rodrigoferrer30@gmail.com>
2025-01-28 15:10:03 -0300

    0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """shebang"""
+
+
 class Square:
     """ Esta es una clase sin atributos aun
-
-
     por eso uso comillas triples para poder poner comentarios estilo doscstring sin nueva linea """
     pass
