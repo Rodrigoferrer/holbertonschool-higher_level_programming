c558459d73508cda7fa6b2d72caf80eab4ce6d37
Rodrigo <rodrigoferrer30@gmail.com>
2025-01-28 09:53:44 -0300

    9

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    for i in a_dictionary:
-        a_dictionary[i] = a_dictionary[i] * 2
-    return a_dictionary
-    
+    copy_dictionary = a_dictionary.copy()
+    for i in copy_dictionary:
+        copy_dictionary[i] = copy_dictionary[i] * 2
+    return copy_dictionary
+
