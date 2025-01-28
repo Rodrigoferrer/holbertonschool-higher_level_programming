c3c3eaa1aa339c0725d1dd73367c2d225e694769
Rodrigo <rodrigoferrer30@gmail.com>
2025-01-28 15:04:35 -0300

    0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 class Square:
+    """ Esta es una clase sin atributos aun
+
+
+    por eso uso comillas triples para poder poner comentarios estilo doscstring sin nueva linea """
     pass
 
     
