cf63ebb8e2083a23e734c49fe390afd17b8c109d
Rodrigo <rodrigoferrer30@gmail.com>
2025-01-29 12:59:24 -0300

    01

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """ Alta clase tiene este objeto cuadrado """
 
+
 class Square:
     """ Siguen las clases  """
     def __init__(self, size):
-            self.__size = size
+        self.__size = size
