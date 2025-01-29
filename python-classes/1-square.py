e1570c7b8eb0d60c193eb4929c979842da947f4b
Rodrigo <rodrigoferrer30@gmail.com>
2025-01-29 12:50:43 -0300

    01

---
+++ python-classes/1-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+class Square:
+    """ Esta es una clase sin atributos aun
+    por eso uso comillas triples para poder poner comentarios estilo doscstring sin nueva linea """
+    def __init__(self, size):
+            self.__size = size
+    
