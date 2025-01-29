490d1645d836dbb8f253005e4370035c89a56f27
Rodrigo <rodrigoferrer30@gmail.com>
2025-01-29 13:44:13 -0300

    02

---
+++ python-classes/2-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Alta clase tiene este objeto cuadrado """
+
+
+class Square:
+    """ Siguen las clases  """
+    def __init__(self, size):
+        if self.size != int:
+            raise TypeError("Size must be an integer")
+        if self.size < 0:
+            raise ValueError("Size must be >= 0")
+        self.__size = size
