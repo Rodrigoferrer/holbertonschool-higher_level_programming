6f80c6f1ab31954b7faf1f2d04cf386fffe122ab
Rodrigo <rodrigoferrer30@gmail.com>
2025-01-30 10:41:52 -0300

    03

---
+++ python-classes/3-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""A class Square that defines a square by size"""
+
+
+class Square:
+    """Square class with private size attribute"""
+    def __init__(self, size=0):
+        """Initialize square with optional size
+
+        Args:
+            size (int, optional): Size of square side. Defaults to 0..
+
+        Raises:
+            TypeError: If size is not an integer
+            ValueError: If size is less than 0
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+        return None
+        def __init__ area(self, area):   
+            self.area = area
+
+        
