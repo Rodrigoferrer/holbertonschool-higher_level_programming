394bcf63da76c26eed06ca310497cb342ec091eb
Rodrigo <rodrigoferrer30@gmail.com>
2025-01-30 14:32:44 -0300

    05

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -44,7 +44,7 @@ class Square:
         return self.__size * self.__size
     
     def my_print(self):
-        """"Prints hasthags""""
+        """Prints hasthags"""
         if self.size == 0:
             print("")
             return
