af23aff828ea26d34ae51a38ebfc1b932f62e27f
Rodrigo <rodrigoferrer30@gmail.com>
2025-01-30 11:47:08 -0300

    02

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -20,6 +20,6 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
         return None
-        
+
     def area(self):
         return self.__size * self.__size
