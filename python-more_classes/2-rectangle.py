fce8ce0e500cbdc4a8f3b83a9e2c187039e654df
Rodrigo <rodrigoferrer30@gmail.com>
2025-02-03 13:19:51 -0300

    2

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -12,8 +12,8 @@ class Rectangle:
             width (int): El ancho del rectángulo. Por defecto es 0.
             height (int): La altura del rectángulo. Por defecto es 0.
         """
-        self.width = width
-        self.height = height
+        self.__width = width
+        self.__height = height
 
     @property
     def width(self):
@@ -35,3 +35,45 @@ class Rectangle:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Obtiene la altura del rectángulo."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Establece la altura del rectángulo.
+
+        Args:
+            value (int): La nueva altura del rectángulo.
+
+        Raises:
+            TypeError: Si la altura no es un entero.
+            ValueError: Si la altura es menor que 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Calcula el área del rectángulo.
+
+        Returns:
+            int: El área del rectángulo.
+        """
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Calcula el perímetro del rectángulo.
+
+        Returns:
+            int: El perímetro del rectángulo, o 0 si el ancho o la altura son 0.
+        """
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
