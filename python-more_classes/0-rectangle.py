50965c1b53ac96572b5a9e5e7721e8a85b6930ea
Rodrigo <rodrigoferrer30@gmail.com>
2025-02-03 09:39:21 -0300

    0

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
+"""script to be executed in python3 env"""
+
 class Rectangle:
     """A class that defines a rectangle."""
     pass
-
-if __name__ == "__main__":
-    print("Rectangle class defined.")
