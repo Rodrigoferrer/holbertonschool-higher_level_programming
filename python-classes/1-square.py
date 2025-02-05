0743dc838ea185ff2e55ee308a76275d49fc63da
Rodrigo <rodrigoferrer30@gmail.com>
2025-02-05 14:46:27 -0300

    1

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,8 +1,34 @@
 #!/usr/bin/python3
-""" Alta clase tiene este objeto cuadrado """
+"""script to be executed in python3 env"""
 
 
-class Square:
-    """ Siguen las clases  """
-    def __init__(self, size):
-        self.__size = size
+class Rectangle:
+    """A class that defines a rectangle."""
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
