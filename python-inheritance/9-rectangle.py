94aca6327812a0cbd657606df715748bab2ec538
Rodrigo <rodrigoferrer30@gmail.com>
2025-02-06 14:48:50 -0300

    9

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Python3"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+class Rectangle(BaseGeometry):
+
+    """class Rectangle"""
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+    
+    def area(self):
+        """Return the area of the rectangle"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Return the string representation of the rectangle"""
+        return f"[Rectangle] {self.__width}/{self.__height}"
