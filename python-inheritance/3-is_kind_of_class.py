0413ad0a889000d7cd8d77c1c61f27a68ef382c6
Rodrigo <rodrigoferrer30@gmail.com>
2025-02-06 11:40:05 -0300

    3

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Python3"""
+
+
+def is_kind_of_class(obj, a_class):
+    """class"""
+
+    class_object = type(obj)
+
+    if class_object isinstance(a_class):
+        return True
+    else:
+        return False
