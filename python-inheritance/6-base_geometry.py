d440c616f492e2e3049381e750e06e58b596dffc
Rodrigo <rodrigoferrer30@gmail.com>
2025-02-06 12:58:32 -0300

    6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Python3"""
+
+
+class BaseGeometry:
+    """Class for base geometry"""
+    def __init__(self):
+        return
+    def area(self):
+        raise Exception("area() is not implemented")
