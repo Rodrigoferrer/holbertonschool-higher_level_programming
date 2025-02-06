2fb6d58a5aa91bb0d7b96b8c68eddea14c252635
Rodrigo <rodrigoferrer30@gmail.com>
2025-02-06 12:02:58 -0300

    3

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -5,7 +5,7 @@
 def inherits_from(obj, a_class):
     """class"""
 
-    if isinstance(obj, a_class):
+    if isinstance(obj, a_class) and type(obj) is not a_class:
         return True
     else:
         return False
