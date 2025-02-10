e4c99b6cb4fff28266d4c2306489168defb6a66f
Rodrigo <rodrigoferrer30@gmail.com>
2025-02-09 23:45:53 -0300

    task 2

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,25 +1,13 @@
-def list_division(my_list_1, my_list_2, list_length):
-    result = []
-    
-    for i in range(list_length):
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    for i in range(x):
         try:
-            if i >= len(my_list_1) or i >= len(my_list_2):
-                print("out of range")
-                result.append(0)
-            else:
-                try:
-                    if not isinstance(my_list_1[i], (int, float)) or not isinstance(my_list_2[i], (int, float)):
-                        print("wrong type")
-                        result.append(0)
-                    else:
-                        result.append(my_list_1[i] / my_list_2[i])
-                except ZeroDivisionError:
-                    print("division by 0")
-                    result.append(0)
-        except Exception as e:
-            print("An unexpected error occurred:", e)
-            result.append(0)
-        finally:
-            pass
-
-    return result
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
+        except (ValueError, TypeError):
+            continue
+        except IndexError:
+            break
+    print()
+    return count
