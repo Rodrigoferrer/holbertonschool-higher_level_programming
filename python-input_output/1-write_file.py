6ad16a1448f72b1e2591d5b67d4c83ef1b3953ae
Rodrigo <rodrigoferrer30@gmail.com>
2025-02-11 11:12:22 -0300

    task 1

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Python3"""
+
+def write_file(filename="", text=""):
+     """Read, modify and print the content of a file.
+
+    Args:
+        filename (str): The name of the file to read.
+        text (str): the text to add
+    """
+    
+    with open(filename, 'r+', encoding='utf-8') as text1:
+        stringtofile = text1.write(text)
+        print(stringtofile)
