1d26873920e197cf8c7f62e98c1b2b753bcdf1a4
Rodrigo <rodrigoferrer30@gmail.com>
2025-02-16 19:40:45 -0300

    task 3

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -4,5 +4,7 @@
 import json
 
 def to_json_string(my_obj):
+    """Function that takes an object of python and converts it to json"""
+    
     my_obj_cpy = json.dumps(my_obj)
     return my_obj_cpy
