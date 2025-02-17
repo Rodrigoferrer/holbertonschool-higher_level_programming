6429284c3a0a74d962c5c425bf9a3b8ff0571fab
Rodrigo <rodrigoferrer30@gmail.com>
2025-02-16 22:47:48 -0300

    task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 """Python3"""
 
+
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
 
 filename = "add_item.json"
 
 try:
     items = load_from_json_file(filename)
+
 except FileNotFoundError:
     items = list
 
