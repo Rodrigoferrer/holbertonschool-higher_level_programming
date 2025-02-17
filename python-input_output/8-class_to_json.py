d0753f608cdad0fa9e1f97ad870e2d16c7b03a02
Rodrigo <rodrigoferrer30@gmail.com>
2025-02-16 23:04:50 -0300

    task 8

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Python3"""
+
+
+def class_to_json(obj):
+    """Return the dictionary description with simple data structure
+    for JSON serialization of an object."""
+
+    json_dict = {}
+    
+    for key, value in obj.__dict__.items():
+        if isinstance(value, (list, dict, str, int, bool)):
+            json_dict[key] = value
+            
+    return json_dict
