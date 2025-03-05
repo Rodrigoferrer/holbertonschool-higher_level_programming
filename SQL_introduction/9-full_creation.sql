ee23b3c9323b0a9706937f65c79888195bc37f21
Rodrigo <rodrigoferrer30@gmail.com>
2025-03-05 11:34:36 -0300

    CREATE NEW TABLE INTO DATABASE

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,2 +1,14 @@
 -- Create second table into hbtn_0c_0
-INSERT IF NOT EXISTS TABLE second_table INTO hbtn_0c_0;
+USE hbtn_0c_0;
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT,
+    name VARCHAR(256),
+    score INT
+);
+
+-- Insert multiple rows into the table
+INSERT INTO second_table (id, name, score) VALUES
+(1, 'John', 10),
+(2, 'Alex', 3),
+(3, 'Bob', 14),
+(4, 'George', 8);
