5e27e632bb688fbcc99dc2c132fa00b990e34c1a
Rodrigo <rodrigoferrer30@gmail.com>
2025-03-10 14:08:00 -0300

    1

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,9 @@
+
 -- Create a user
-CREATE  USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT SELECT ALL PRIVILEGES *.* ON 'user_0d_1'@'localhost' WITH GRANT OPTION;
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant privileges to the user
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+
+-- Flush privileges to ensure that the changes take effect
 FLUSH PRIVILEGES;
