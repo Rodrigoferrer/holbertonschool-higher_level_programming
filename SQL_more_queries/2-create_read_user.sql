4f1ad207216cd34d8ad246500a134073b8f20598
Rodrigo <rodrigoferrer30@gmail.com>
2025-03-10 14:45:24 -0300

    2

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,5 @@
+-- create database and user
+CREATE IF NOT EXISTS DATABASE hbtn_0d_2;
+CREATE IF NOT EXISTS USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
