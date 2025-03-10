471ba3488b0a6d3bbc987fdb1505a3aad47db4bd
Rodrigo <rodrigoferrer30@gmail.com>
2025-03-10 14:59:58 -0300

    5

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,2 +1,2 @@
 -- Create new table
-CREATE TABLE IF NOT EXISTS force_name(id INT DEFAULT 1, name VARCHAR(256) NOT NULL, UNIQUE (id));
+CREATE TABLE IF NOT EXISTS unique_id(id INT DEFAULT 1, name VARCHAR(256) NOT NULL, UNIQUE (id));
