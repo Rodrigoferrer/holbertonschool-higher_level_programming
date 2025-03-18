60428c054b583c4fe8d74a1a6bb73089588075f4
Rodrigo <rodrigoferrer30@gmail.com>
2025-03-18 12:34:11 -0300

    task 0 ORM

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,19 @@
+import Mysqldb
+
+username = 'rodrigo'
+password = 'admin'
+database = 'hbtn_0e_0_usa'
+
+db = MySQLdb.connect(host="localhost", user=username, passwd=password, db=database, port=3306)
+
+cursor = db.cursor()
+
+cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+results = cursor.fetchall()
+
+for row in results:
+    print(row)
+
+cursor.close()
+db.close()
