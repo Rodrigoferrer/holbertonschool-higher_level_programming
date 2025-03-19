759d5b69c98438007cd1ff7b3804b2b9859acc22
Rodrigo <rodrigoferrer30@gmail.com>
2025-03-19 09:36:46 -0300

    5-filter_cities.py

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -24,9 +24,11 @@ if __name__ == "__main__":
 
     cursor = db.cursor()
 
-    query = ("""SELECT cities.id, cities.name, states.name
+    query = ("""
+        SELECT cities.name
         FROM cities
         JOIN states ON cities.state_id = states.id
+        WHERE states.name = %s
         ORDER BY cities.id ASC
         """)
 
